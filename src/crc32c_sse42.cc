4a08c9b1965fa92c4dc4b0be207af4aba03d3e87
Victor Costan <pwnall@chromium.org>
2017-08-30 15:18:11 -0700

    Fixed copyright year for imported code.

--- src/crc32c_sse42.cc
+++ src/crc32c_sse42.cc
@@ -1,4 +1,4 @@
-// Copyright 2017 The CRC32C Authors. All rights reserved.
+// Copyright 2008 The CRC32C Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
 
